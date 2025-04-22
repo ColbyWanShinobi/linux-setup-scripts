1d66dcb8631e7ade2cafd74b16e0c4c8d8a13980
Colby Butler <colby@owljet.com>
2025-04-21 23:15:23 -0400

    New extension

--- install-gnome-extensions.sh
+++ install-gnome-extensions.sh
@@ -58,6 +58,7 @@ extlist=(
 "https://extensions.gnome.org/extension/5362/wireguard-vpn-extension/"
 "https://extensions.gnome.org/extension/5964/quick-settings-audio-devices-hider/"
 "https://extensions.gnome.org/extension/1183/utcclock/"
+"https://extensions.gnome.org/extension/3733/tiling-assistant/"
 )
 
 CURRENT_GNOME_VERSION=$(gnome-shell --version | grep -oP '\d+\.\d+' | head -1 | cut -f1 -d.)
